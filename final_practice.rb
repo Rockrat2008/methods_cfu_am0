b003759dba50311b6f7f67048158c2da8b9949b5
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-07 14:08:16 -0400

    Completes the first 3 challenges

--- final_practice.rb
+++ final_practice.rb
@@ -22,8 +22,20 @@ puts last_laugh
 # Use the puts or print command to see the return value in the console:
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
+# "What's up my friend?"
 
+# How many arguments did you pass your method?
+# 0
+
+def greeting
+  "What's up my friend"
+end
+
+first_greeting = greeting
+second_greeting = greeting
+
+puts first_greeting
+puts second_greeting
 
 # 2: Write a method named "custom_greeting" that returns a greeting WITH a specific name.
 
@@ -31,10 +43,19 @@ puts last_laugh
 
 # Use the puts or print command to see the return value in the console:
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? The created custom greeting with the argument that was passed
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)? - string
 
+def custom_greeting(user_name)
+  "What's up #{user_name}"
+end
+
+custom_greeting1 = custom_greeting("Michael")
+puts custom_greeting1
+
+custom_greeting2 = custom_greeting("Thomas")
+puts custom_greeting2
 
 # 3: Write a method named "greet_person" that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
@@ -42,10 +63,19 @@ puts last_laugh
 
 # Use the puts or print command to see the return value in the console:
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? A string with the 3 arguments
+# How many arguments did you pass your method? 3
+# What data type was your argument(s)? string
 
+def greet_person(first_name, middle_name, last_name)
+  "#{first_name} #{middle_name} #{last_name}"
+end
+
+full_name1 = greet_person("Michael", "Thomas", "OBrien")
+puts full_name1
+
+full_name2 = greet_person("Tammy", "Lanette", "Ballard")
+puts full_name2
 
 # 4: Write a method named "square" that takes in one integer, and returns the square of that integer.
 
